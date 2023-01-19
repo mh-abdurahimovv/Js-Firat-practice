0ab77e78487a87bf107fc240e3c818db976fd365
mh-abdurahimovv <121340389+mh-abdurahimovv@users.noreply.github.com>
2023-01-19 10:22:20 +0300

    added js
    full js course

---
+++ Full Js/01_bases.js
@@ -0,0 +1,160 @@
+// const result = confirm('Are you here?' )
+// console.log(result)
+
+// const answer = prompt('Вам есть 18?', '20')
+// console.log(answer)
+
+
+// const answer = +prompt('Вам есть 18?', '20')
+// console.log(answer + 5)
+
+
+// const answers  = []
+// answers[0] = prompt('What is your name?', ' ')
+// answers[1] = prompt('What is your surname?', ' ')
+// answers[2] = prompt('How old are you?', ' ')
+// //document.write(answers)
+//  console.log(typeof(answers))
+
+
+// const category = 'toys'
+// console.log(`https://somurl.com/${category}/info/about`)
+
+
+// const name = 'Huseyn'
+// alert(`Hi, ${name}`)
+
+// const user = 'Abdurahimov'
+// alert(`Hi, ${user}`)
+
+
+// ОПЕРАТОРЫ JS
+
+// let incr = 10,
+//     decr = 15;
+
+// Перед значениями = Префиксные
+// incr++;
+// decr--;
+
+// После значений = Постфиксными
+// ++incr;
+// --decr;
+// console.log(incr)    
+// console.log(decr)
+
+
+////// =================
+/*
+Один раз написать знак равно = означает присваивание
+console.log(a = 5)
+
+Два раза написать знак равно == означает сравнение
+console.log(2 * 4 == 8)      //true
+console.log(2 * 4 == '8')   //true
+
+Три раза написать знак равно === означает строгое сравнение
+console.log(2 * 4 === '8')   //False
+console.log(2 * 4 === 8)    //true
+
+*/
+
+
+// Оператор отрецание (!) 
+// const isChecked = false,
+//       isClose = false;
+// console.log(isChecked || !isClose); // так как перед вторым
+// значением мы добавили Оператор отрецание (воскресательный знак), оно превратилось в true
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
